4f378ad2a2bc14d8f0ba974ae8b76a46434b51cd
dextreydev <anirlachguer120@gmail.com>
2026-07-25 19:03:10 +0100

    Update fly.lua

--- fly.lua
+++ fly.lua
@@ -1,89 +1,101 @@
+----------------------------------------------------
+---  A redistribution of https://wearedevs.net/  ---
+----------------------------------------------------
+
+--Waits until the player is in game
+repeat wait()
+until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Torso") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+--Waits until the player's mouse is found
+repeat wait() until mouse
+
+--Variables
 local plr = game.Players.LocalPlayer
-local char = plr.Character or plr.CharacterAdded:Wait()
-local torso = char.Torso
-if not torso then torso = char:WaitForChild("Upper Torso") end
-local humanoid = char:WaitForChild("Humanoid")
+local torso = plr.Character.Torso
+local flying = true
+local deb = true
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+local maxspeed = 50
+local speed = 0
+local bg = nil
+local bv = nil
 
-local flyingEnabled = false
-local ctrl = {f=0,b=0,l=0,r=0}
+--Actual flying
+function Fly()
+	game.StarterGui:SetCore("SendNotification", {Title="Fly Activated"; Text="WeAreDevs.net"; Duration=1;})
+    bg = Instance.new("BodyGyro", torso)
+    bg.P = 9e4
+    bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+    bg.cframe = torso.CFrame
+    bv = Instance.new("BodyVelocity", torso)
+    bv.velocity = Vector3.new(0,0.1,0)
+    bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+    repeat wait()
+      plr.Character.Humanoid.PlatformStand = true
+      if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+        speed = speed+.5+(speed/maxspeed)
+        if speed > maxspeed then
+          speed = maxspeed
+        end
+      elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+        speed = speed-1
+        if speed < 0 then
+          speed = 0
+        end
+      end
+      if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+        lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+      elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+      else
+        bv.velocity = Vector3.new(0,0.1,0)
+      end
+      bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
+    until not flying
+    ctrl = {f = 0, b = 0, l = 0, r = 0}
+    lastctrl = {f = 0, b = 0, l = 0, r = 0}
+    speed = 0
+    bg:Destroy()
+	bg = nil
+    bv:Destroy()
+	bv = nil
+    plr.Character.Humanoid.PlatformStand = false
+	game.StarterGui:SetCore("SendNotification", {Title="Fly Deactivated"; Text="WeAreDevs.net"; Duration=1;})
+end
 
--- Create BG & BV once
-local bg = Instance.new("BodyGyro")
-bg.MaxTorque = Vector3.new(9e9, 9e9, 9e9)
-bg.P = 9e4
-bg.Parent = torso
-
-local bv = Instance.new("BodyVelocity")
-bv.MaxForce = Vector3.new(9e9, 9e9, 9e9)
-bv.Velocity = Vector3.new(0, .1, 0)
-bv.Parent = torso
-
--- Movement function
-game:GetService("RunService").RenderStepped:Connect(function()
- if flyingEnabled then
- local camCFRameLookVectorForward;
- if ctrl.f == 1 then 
- camCFRameLookVectorForward += workspace.CurrentCamera.CoordinateFrame.lookVector  10 
- end
- 
- local camCFRameRight;
- if ctrl.l == -1 then  
- camCFRameRight -= workspace.CurrentCamera.CoordinateFrame.rightVector  -10  
- 
-		-- Adjust height based on Spacebar press (up).
-		elseif ctrl.r == 1 then  
- camCFRameRight += workspace.CurrentCamera.CoordinateFrame.rightVector  -10  
-
+--Controls
+mouse.KeyDown:connect(function(key)
+	if key:lower() == "e" then
+		if flying then 
+			flying = false
+		else
+			flying = true
+			Fly()
 		end
- 
- 
- -- Calculate final velocity vector combining all movements.
- local finalMovementDirection;
- if not camCFRameLookVectorForward then  
- finalMovementDirection += (camCFRameLookVectorForward + camCFRameRight)
- 
-		else  
- finalMovementDirection += (camCFRameLookVectorForward + camCFRameRight)
- 
-		end
- 
- 
- else 
- bv.Velocity=vector3.zero 
- bg.CFrame=torso.CFrame 
- end 
+	elseif key:lower() == "w" then
+		ctrl.f = 1
+	elseif key:lower() == "s" then
+		ctrl.b = -1
+	elseif key:lower() == "a" then
+		ctrl.l = -1
+	elseif key:lower() == "d" then
+		ctrl.r = 1
+	end
 end)
 
--- Input handling
-game:GetService("UserInputService").InputBegan:Connect(function(inputObject)
- if inputObject.KeyCode == Enum.KeyCode.E then 
- flyingEnabled=not(flyingEnabled) 
- elseif inputObject.KeyCode==Enum.KeyCode.W then 
- ctrl.f=1 
- 
- elseif inputObject.KeyCode==Enum.KeyCode.S then 
- ctrl.b=-1 
- 
- elseif inputObject.KeyCode==Enum.KeyCode.A then 
- ctrl.l=-1 
- 
- elseif inputObject.KeyCode==Enum.KeyCode.D then 
- ctrl.r=1 
-	end 
+mouse.KeyUp:connect(function(key)
+	if key:lower() == "w" then
+		ctrl.f = 0
+	elseif key:lower() == "s" then
+		ctrl.b = 0
+	elseif key:lower() == "a" then
+		ctrl.l = 0
+	elseif key:lower() == "d" then
+		ctrl.r = 0
+	end
 end)
 
-game:GetService("UserInputService").InputEnded:Connect(function(inputObject)	
-	if inputObject.KeyCode==Enum.KeyCode.W then 
-		ctrl.f=0 
-		
-	elif inputObject.KeyCode==Enum.KeyCode.S then 
-		ctrl.b=-0 
-		
-	elif inputObject.KeyCode==Enum.KeyCode.A then 
-	(ctrl.l=-0) 
-		
-	elif inputObject(KeyCode.D)==true(control.r=-0) 
-		
-	end	
-end)
-
+Fly()
